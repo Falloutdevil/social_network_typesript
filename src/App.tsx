ad71a84b7c16bef507455f4a8528776f056cc313
Falloutdevil <Falloutdevil@ya.ru>
2023-11-14 17:07:41 +0300

    grid (11)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,44 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-    <div className="App">
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>js</li>
-        </ul>
+    <div className="app-wrapper">
+        <header className="header"><img src="https://cdn-icons-png.flaticon.com/512/36/36447.png" alt="logo"/>
+        </header>
+        <nav className="nav">
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Message</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+        <div className="content">
+            <div>
+                <img className="coverImg"
+                     src="https://s13.stc.all.kpcdn.net/russia/wp-content/uploads/2019/11/Altajskie-gory-oblozhka-1300x415.jpg"
+                     alt=""/>
+            </div>
+            <div> ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
     </div>
 );
 
