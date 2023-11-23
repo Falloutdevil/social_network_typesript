008c24184c450ae09f8c3cfbd055bcb1884bd80c
Falloutdevil <Falloutdevil@ya.ru>
2023-11-23 19:18:24 +0300

    dialogs (18)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import './App.css';
 import {Header} from './components/header/Header';
 import {Navbar} from './components/navbar/Navbar';
 import {Profile} from './components/profile/Profile';
+import {Dialogs} from './components/dialogs/Dialogs';
 
 const App = () => (
     <div className="app-wrapper">
         <Header/>
         <Navbar/>
-        <Profile/>
+        <div className='app-wrapper-content'>
+            {/*<Profile/>*/}
+            <Dialogs />
+        </div>
+
     </div>
 );
 
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from './myPosts/MyPosts';
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img className={s.coverImg}
                      src="https://s13.stc.all.kpcdn.net/russia/wp-content/uploads/2019/11/Altajskie-gory-oblozhka-1300x415.jpg"
