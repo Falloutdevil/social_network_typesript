ccc4261d293e23ef31dd9e5c2c9dfd1d210cdd51
Falloutdevil <Falloutdevil@ya.ru>
2023-11-23 20:42:56 +0300

    route, browser-router (19)

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import {Header} from './components/header/Header';
 import {Navbar} from './components/navbar/Navbar';
 import {Profile} from './components/profile/Profile';
 import {Dialogs} from './components/dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => (
     <div className="app-wrapper">
-        <Header/>
-        <Navbar/>
-        <div className='app-wrapper-content'>
-            {/*<Profile/>*/}
-            <Dialogs />
-        </div>
-
+        <BrowserRouter>
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Route path='/profile' component={Profile}/>
+                <Route path='/dialogs' component={Dialogs}/>
+            </div>
+        </BrowserRouter>
     </div>
 );
 
