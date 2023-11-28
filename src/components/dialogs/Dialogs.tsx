10db66976acfc40393655dea99abf983736cb75a
Falloutdevil <Falloutdevil@ya.ru>
2023-11-28 21:38:50 +0300

    page dialogs (21)

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Pasha
+                </div>
+                <div className={s.dialog}>
+                    Nick
+                </div>
+                <div className={s.dialog}>
+                    Nina
+                </div>
+                <div className={s.dialog}>
+                    Denis
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is you Ok?</div>
+                <div className={s.message}>Nice!</div>
+            </div>
         </div>
     );
 };
