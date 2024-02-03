4092fe6d89ece6ad51e5f080a58a0c3890d4c21d
Falloutdevil <Falloutdevil@ya.ru>
2024-02-03 18:55:20 +0300

    onClick, ref, VirtualDOM (31)

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -7,23 +7,29 @@ type MyPostsType = {
     posts: PostType[]
 }
 
-export const MyPosts:React.FC<MyPostsType> = (props) => {
-    const {posts} = props;
+export const MyPosts: React.FC<MyPostsType> = ({posts}) => {
     let postElements = posts.map(p => {
         return (
             <Post key={p.id} id={p.id} message={p.message} likesCount={p.likesCount}/>
         )
     })
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const addPost = () => {
+        let text = newPostElement.current?.value;
+        alert(text);
+    }
+
     return (
         <div className={s.postBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
